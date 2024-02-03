18099aefed4438bfc6a0e010d865c8260ae3271f
nitin1kum <143751900+nitin1kum@users.noreply.github.com>
2024-02-03 20:24:37 +0530

    chaging router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Layout from './Components/layout';
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
